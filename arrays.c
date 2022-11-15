c44778b384ca2dfb002ca161216058a0a2e385e4
bonkie <shlokedu09@gmail.com>
2022-11-15 13:43:29 +0530

    learnt arrays

---
+++ arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+  int i=0;
+  for(i=1;i<argc;i++){
+    printf("arg %d:%s \n",i,argv[i]);
+  }
+  char* states[]={
+    "Calafornia","Bonk"
+  };
+  int numStates=2;
+  for(i=0;i<numStates;i++){
+    printf("States:%s \n",states[i]);
+  }
+
+  return 0;
+}
+
+
+
+
