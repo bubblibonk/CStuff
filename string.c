0a79efaaf5497d21fad8759d8e7a264c3d30f649
bonkie <shlokedu09@gmail.com>
2022-11-16 13:21:10 +0530

    did strings and learnt how to breakk it wit '\0'

--- string.c
+++ string.c
@@ -2,14 +2,15 @@
 void printStr(char str[]){
   int i=0;
   while(str[i]!='\0'){
-    printf("%c \n",str[i]);
+    printf("%c",str[i]);
     i++;
   }
 }
 int main (int argc, char *argv[])
 {
-  char str[]={'b','o','n','k'};
+  char str[4]={'b','o','n','k'};
+  str[4]='a';//this line breaks it huehue
   //printStr(str);
-  printf("%s\n",str);
+  printf("%s \n",str);
   return 0;
 }
