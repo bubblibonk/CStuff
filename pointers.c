8af77a060816d57e9ee955d5d5dcd96e6b2568fd
bonkie <shlokedu09@gmail.com>
2022-11-18 22:52:26 +0530

    pointers goo weee

--- pointers.c
+++ pointers.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-int main(int argc ,char *argv[]){
-  int x = 4;
-  int *pX=&x;
-  int y =*pX;
 
-  printf("%p \n",pX);
-  
-  printf("%d \n",y);
-  
+int main (int argc, char *argv[])
+{
+
+  int ages[]={1,2,3,41};
+
+  int *pAges=ages;
+  int i =0;
+  int count = sizeof(ages) / sizeof(int);
+  for (i=0;i<count;i++){
+    printf("%d : %p \n",pAges[i],pAges);
+  }
+  return 0;
 }
